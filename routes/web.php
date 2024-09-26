6c79ccd3ccc92ccca2be43951a0c4217dc2c1924
afzal <afzalbhola07@gmail.com>
2024-09-26 23:30:34 +0600

    User Dashboard Setup Done

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\customer\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Admin Home Route Section Start //
+Route::controller(CustomerController::class)->group(function () {
+    Route::get('/test', 'Customer_Dashboard')->name('customer');
+});
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -28,4 +35,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
